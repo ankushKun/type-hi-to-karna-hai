a46c2482694b0fba572bbb18159fd45fcf392beb
ankushKun <ankush4singh@gmail.com>
2023-10-21 10:21:55 +0530

    typo

--- nextapp/src/pages/index.tsx
+++ nextapp/src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <Image alt="gitraven" src={eth} width={500} height={500} className="rounded-full shadow-teal-500 shadow-lg" />
         </div>
         <div className="w-1/2 min-w-[50%]">
-          <div className="font-bold text-4xl m-5 leading-relaxed"><span className="text-6xl">Boost your <span className="text-[#FFD600]">income</span></span><br /> while boosting open source quality become a <span className="text-[#FFD600]">open-sourcer</span>!</div>
+          <div className="font-bold text-4xl m-5 leading-relaxed"><span className="text-6xl">Boost your <span className="text-[#FFD600]">income</span></span><br /> while boosting open source quality become an <span className="text-[#FFD600]">open-sourcer</span>!</div>
           <Link href="/bounties" className="bg-[#FFD600] text-black font-bold text-3xl p-2 px-5 realative top-5 rounded-full">Explore Bounties</Link>
         </div>
       </div>
