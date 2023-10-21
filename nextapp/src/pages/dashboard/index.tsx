2cdcba2dc1eb332bcb7508a2a8a1098e5e98e433
ankushKun <ankush4singh@gmail.com>
2023-10-21 13:38:55 +0530

    dashboard wip

--- nextapp/src/pages/dashboard/index.tsx
+++ nextapp/src/pages/dashboard/index.tsx
@@ -8,12 +8,12 @@ import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function dashIndex() {
     const [connected, setConnected] = useState(false)
+    const [showNewBountyPopup, setShowNewBountyPopup] = useState(false)
     const { isConnected, address } = useAccount()
     const { disconnect } = useDisconnect()
     const { connect } = useConnect()
     const router = useRouter()
     const { data: session } = useSession()
-
 
     useEffect(() => {
         setConnected(isConnected)
@@ -21,10 +21,14 @@ export default function dashIndex() {
     }, [isConnected])
 
     return <Page>
-        <div>
+        {showNewBountyPopup && <div>
+
+        </div>}
+        <div className="flex flex-col justify-center items-center h-[70vh] gap-3">
             <div className="text-3xl mb-5">Dashboard</div>
             <div>Wallet Connected : {connected ? <>{address} <button onClick={() => disconnect()} className="ml-2 ring-1 p-1 px-2 ring-white rounded-lg">signout</button></> : <button className="ml-2 ring-1 p-1 px-2 ring-white rounded-lg" onClick={() => connect()}>connect</button>}</div>
             {session ? <div className="flex justify-center"><div>Github: {session?.user?.login}</div> <button className="ml-2 ring-1 p-1 px-2 ring-white rounded-lg" onClick={() => signOut()}>disconnect</button></div> : <div> <button className="ring-1 ring-white p-1 px-2 rounded-lg" onClick={() => signIn("github")}>connect github</button></div>}
+            <button className="" onClick={() => setShowNewBountyPopup(true)}>new bounty</button>
         </div>
     </Page>
 }
